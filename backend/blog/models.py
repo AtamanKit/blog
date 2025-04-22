f6065e9a3d83464db43157255c9ac479e6388b71
AtamanKit <rodionatamaniuc@gmail.com>
2025-04-22 20:55:45 +0300

    Added @tailwindcss/typography

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django_ckeditor_5.fields import CKEditor5Field
+from django_ckeditor_5.fields import CKEditor5Field
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='post_images', blank=True, null=True)
-    # content=CKEditor5Field(config_name='extends')
-    content = models.TextField()
+    content=CKEditor5Field(config_name='extends')
+    # content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
