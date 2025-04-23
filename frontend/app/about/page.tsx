62c2939be2c693c7b760fc7e2b7b17f62cad6729
AtamanKit <rodionatamaniuc@gmail.com>
2025-04-23 12:00:03 +0300

    Changed the aboutme page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -6,12 +6,12 @@ export default function AboutPage() {
     return (
         <div className="mx-auto max-md:px-6 py-12">
             <BreadcrumbTemplate param="About Me" />
-            <h1 className="text-4xl font-bold text-center mb-6">Hey there! My name is Rodion Atamaniuc 👋</h1>
+            <h1 className="text-4xl font-bold text-center mb-6">Hey there! My name is Rodion Atamaniuc</h1>
             <p className="text-lg text-center mb-8">
                 Full-Stack Developer | Python, JavaScript | DevOps Enthusiast
             </p>
             <div className="space-y-6 flex flex-wrap md:flex-nowrap items-start">
-                <Image 
+                <Image
                     src="/images/jpg/about.jpg"
                     alt="Rodion Atamaniuc"
                     width={330}
@@ -29,31 +29,32 @@ export default function AboutPage() {
                         But my passion doesn’t stop at development—I’m also into <strong>DevOps</strong>, making sure everything runs smoothly with
                         <span className="text-blue-600"> Docker, Docker Compose and Kubernetes</span>.
                     </p>
-                    <h2 className="text-2xl font-semibold mt-8">What I Love to Do 🚀</h2>
-                <ul className="list-disc list-inside space-y-2 text-secondary-foreground">
-                    <li><strong>Backend Wizardry:</strong> Crafting robust APIs with Django & FastAPI.</li>
-                    <li><strong>Frontend Magic:</strong> Creating sleek UIs with React.js & Next.js.</li>
-                    <li><strong>Deployment & DevOps:</strong> Making apps scalable with Docker & Kubernetes.</li>
-                    <li><strong>Exploring AI & Data Science:</strong> Digging into AI-powered apps and data-driven projects.</li>
-                </ul>
+                    <h2 className="text-2xl font-semibold mt-8">What I Love to Do</h2>
+                    <ul className="list-disc list-inside space-y-2 text-secondary-foreground">
+                        <li><strong>Backend Wizardry:</strong> Crafting robust APIs with Django & FastAPI.</li>
+                        <li><strong>Frontend Magic:</strong> Creating sleek UIs with React.js & Next.js.</li>
+                        <li><strong>Deployment & DevOps:</strong> Making apps scalable with Docker & Kubernetes.</li>
+                        <li><strong>Exploring AI & Data Science:</strong> Digging into AI-powered apps and data-driven projects.</li>
+                    </ul>
                 </div>
             </div >
-            
-                
 
-                <h2 className="text-2xl font-semibold mt-8">My Tech Toolbox 🧰</h2>
-                <p className="text-secondary-foreground">
-                    <strong>Languages:</strong> Python, JavaScript, TypeScript<br />
-                    <strong>Backend:</strong> Django, FastAPI, Django REST Framework<br />
-                    <strong>Frontend:</strong> React.js, Next.js, TailwindCSS<br />
-                    <strong>Databases:</strong> PostgreSQL, SQLite<br />
-                    <strong>DevOps & Cloud:</strong> Docker, Kubernetes, DigitalOcean<br />
-                    <strong>AI & Data Science:</strong> Pandas, NumPy, TensorFlow (still learning!)
-                </p>
+
+
+            <h2 className="text-2xl font-semibold mt-8">My Tech Toolbox</h2>
+            <p className="text-secondary-foreground">
+                <strong>Languages:</strong> Python, JavaScript, TypeScript<br />
+                <strong>Backend:</strong> Django, FastAPI, Django REST Framework<br />
+                <strong>Frontend:</strong> React.js, Next.js, TailwindCSS<br />
+                <strong>Databases:</strong> PostgreSQL, SQLite<br />
+                <strong>DevOps & Cloud:</strong> Docker, Kubernetes, DigitalOcean<br />
+                <strong>AI & Data Science:</strong> Pandas, NumPy, TensorFlow (still learning!)
+            </p>
 
             <div className="text-center mt-10">
-                <p className="text-lg font-semibold">Let’s Connect! 🤝</p>
+                <p className="text-lg font-semibold">Let’s Connect!</p>
                 <p className="text-secondary-foreground">If you’re looking for a developer who loves building cool things, solving problems, and making the web a better place—hit me up!</p>
+                <p><span className="font-semibold">Email: </span><span className="text-secondary-foreground">atamanpy@gmail.com</span></p>
             </div>
         </div >
     );
